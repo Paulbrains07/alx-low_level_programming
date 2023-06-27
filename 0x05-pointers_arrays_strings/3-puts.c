3b12937dd0f9adea435f60864ec1f4adb3fd6f43
paulbrains07 <paulamba123@gmail.com>
2023-06-27 14:41:19 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Description: _puts - prints a string
+ * @str: The string to print
+ * Return: void
+ **/
+
+void _puts(char *str)
+
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar9(*str);
+	}
+	_putchar9('\n');
+}
